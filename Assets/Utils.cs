ca3ec09109a3964c641d42afe7933693421b0c05
Jason Lane Johnson <jasjohn3@uat.edu>
2020-03-13 00:17:58 -0700

    Unity Build Commit

---
+++ Assets/Utils.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils {
+    //sets our world height for the Perlin Noise
+	static int maxHeight = 150;
+
+	static float smooth = 0.01f;
+    //Sets the number of iterations of perlin noise.
+	static int octaves = 4;
+
+	static float persistence = 0.5f;
+
+	public static int GenerateHeight(float x, float z)
+	{
+        //uses brownian motion fBM function to modify the terrain height.
+		float height = Map(0,maxHeight, 0, 1, fBM(x*smooth/4,z*smooth/4,octaves,persistence));
+		return (int) height;
+	}
+    public static float fBM3D(float x, float y, float z, float sm, int oct)
+    {
+        float XY = fBM(x * sm, y * sm, oct, 0.5f);
+        float YZ = fBM(y * sm, z * sm, oct, 0.5f);
+        float XZ = fBM(x * sm, z * sm, oct, 0.5f);
+
+        float YX = fBM(y * sm, x * sm, oct, 0.5f);
+        float ZY = fBM(z * sm, y * sm, oct, 0.5f);
+        float ZX = fBM(z * sm, x * sm, oct, 0.5f);
+
+        return (XY + YZ + XZ + YX + ZY + ZX) / 6.0f;
+    }
+    public static int GenerateStoneHeight(float x, float z)
+    {
+        //uses brownian motion fBM function to modify the terrain height.
+        float height = Map(0, maxHeight- 20, 0, 1, fBM(x * smooth, z * smooth *2 , octaves + 2, persistence));
+        return (int)height;
+    }
+
+
+    static float Map(float newmin, float newmax, float origmin, float origmax, float value)
+    {
+        return Mathf.Lerp (newmin, newmax, Mathf.InverseLerp (origmin, origmax, value));
+    }
+
+    //fractal brownian motion that modifies the perlin noise for our program.
+    static float fBM(float x, float z, int oct, float pers)
+    {
+        float total = 0;
+        float frequency = 1;
+        float amplitude = 1;
+        float maxValue = 0;
+        for(int i = 0; i < oct ; i++) 
+        {
+                total += Mathf.PerlinNoise(x * frequency, z * frequency) * amplitude;
+
+                maxValue += amplitude;
+
+                amplitude *= pers;
+                frequency *= 2;
+        }
+
+        return total/maxValue;
+    }
+
+}
---
+++ Assets/World.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+    //This variable is where the Textures will be placed for the World.
+	public Material textureAtlas;
+    //Size of the World
+
+	public static int columnHeight = 16;
+
+    public static int chunkSize = 16;
+
+    public static int worldSize = 8;
+    //Dictionary that stores the name and values of our Chunks by Key Value Pairs.
+	public static Dictionary<string, Chunk> chunks;
+
+    //Function that creates a Unique Name based on starting position of the Chunk
+	public static string BuildChunkName(Vector3 v)
+	{
+		return (int)v.x + "_" + 
+			         (int)v.y + "_" + 
+			         (int)v.z;
+	}
+
+    //Constructor for our Columns that Modifies the Height of Construction by multiplying i*chunkSize.
+
+    IEnumerator BuildChunkColumn()
+	{
+		for(int i = 0; i < columnHeight; i++)
+		{
+			Vector3 chunkPosition = new Vector3(this.transform.position.x, 
+												i*chunkSize, 
+												this.transform.position.z);
+			Chunk c = new Chunk(chunkPosition, textureAtlas);
+			c.chunk.transform.parent = this.transform;
+			chunks.Add(c.chunk.name, c);
+		}
+
+        //search for each key value pair in chunks and perform the draw function.
+		foreach(KeyValuePair<string, Chunk> c in chunks)
+		{
+			c.Value.DrawChunk();
+			yield return null;
+		}
+		
+	}
+    IEnumerator  BuildWorld()
+    {
+        for (int z = 0; z < worldSize; z++)
+            for (int x = 0; x < worldSize; x++)
+                for (int y = 0; y < columnHeight; y++)
+                {
+                    Vector3 chunkPosition = new Vector3(x * chunkSize, y * chunkSize, z * chunkSize);
+                    Chunk c = new Chunk(chunkPosition, textureAtlas);
+                    c.chunk.transform.parent = this.transform;
+                    chunks.Add(c.chunk.name, c);
+
+                }
+        //search for each key value pair in chunks and perform the draw function.
+        foreach (KeyValuePair<string, Chunk> c in chunks)
+        {
+            c.Value.DrawChunk();
+            yield return null;
+        }
+
+
+    }
+
+
+        
+
+
+	// Use this for initialization
+	void Start () {
+		chunks = new Dictionary<string, Chunk>();
+		this.transform.position = Vector3.zero;
+		this.transform.rotation = Quaternion.identity;
+		StartCoroutine(BuildWorld());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
